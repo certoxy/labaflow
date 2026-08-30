d4244ba8170effa1a0d0bf6e152f662338994d1a
Glenn Ocay <certoxy@gmail.com>
2026-08-30 11:27:01 +0800

    Add routed Loyalty layout

---
+++ app/loyalty/layout.tsx
@@ -0,0 +1,2 @@
+import SharedSidebar from "../SharedSidebar";
+export default function LoyaltyLayout({children}:{children:React.ReactNode}){return <div className="appShell"><SharedSidebar active="loyalty"/>{children}</div>}
