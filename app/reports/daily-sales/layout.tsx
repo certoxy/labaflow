e229e568da7040777bd71d830217f49ccc594b85
Glenn Ocay <certoxy@gmail.com>
2026-08-30 11:09:13 +0800

    Add Daily Sales report layout

---
+++ app/reports/daily-sales/layout.tsx
@@ -0,0 +1,2 @@
+import SharedSidebar from "../../SharedSidebar";
+export default function DailySalesLayout({children}:{children:React.ReactNode}){return <div className="appShell"><SharedSidebar active="daily-sales"/>{children}</div>}
