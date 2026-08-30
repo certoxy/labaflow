c379c29d30d20ec69a96c038975b526430431f57
Glenn Ocay <certoxy@gmail.com>
2026-08-30 11:26:35 +0800

    Add routed Services layout

---
+++ app/services/layout.tsx
@@ -0,0 +1,2 @@
+import SharedSidebar from "../SharedSidebar";
+export default function ServicesLayout({children}:{children:React.ReactNode}){return <div className="appShell"><SharedSidebar active="services"/>{children}</div>}
