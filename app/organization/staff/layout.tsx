8e1f2d0b8b9f512dd08e7e8288e2a4573329fabb
Glenn Ocay <certoxy@gmail.com>
2026-08-30 12:16:11 +0800

    Add local staff page layout

---
+++ app/organization/staff/layout.tsx
@@ -0,0 +1,2 @@
+import SharedSidebar from "../../SharedSidebar";
+export default function LocalStaffLayout({children}:{children:React.ReactNode}){return <div className="appShell"><SharedSidebar active="staff-access"/>{children}</div>}
