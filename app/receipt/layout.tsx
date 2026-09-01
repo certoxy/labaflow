1026c37b08d45032ebaba3569a44d3084259f2e9
Glenn Ocay <certoxy@gmail.com>
2026-09-01 14:13:24 +0800

    Mount product items on receipt

--- app/receipt/layout.tsx
+++ app/receipt/layout.tsx
@@ -1,5 +1,6 @@
 import SharedSidebar from "../SharedSidebar";
+import ReceiptProducts from "./ReceiptProducts";
 
 export default function ReceiptLayout({children}:{children:React.ReactNode}){
- return <div className="appShell"><SharedSidebar active="orders"/>{children}</div>;
+ return <div className="appShell"><SharedSidebar active="orders"/>{children}<ReceiptProducts/></div>;
 }
