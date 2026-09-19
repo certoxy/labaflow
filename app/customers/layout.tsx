10b13b4b13b18209a71277d90a894b43f2f57867
Glenn Ocay <certoxy@gmail.com>
2026-09-19 12:12:04 +0800

    Load customer branch card styles

--- app/customers/layout.tsx
+++ app/customers/layout.tsx
@@ -1,4 +1,5 @@
 import SharedSidebar from "../SharedSidebar";
+import "./branch-cards.css";
 
 export default function CustomersLayout({children}:{children:React.ReactNode}){
  return <div className="appShell"><SharedSidebar active="customers"/>{children}</div>;
